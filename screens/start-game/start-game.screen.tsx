2477da93fe22910d33edda3b75d30636bfb3bb45
Felipe Rocha <felipemota.rocha@gmail.com>
2020-09-22 19:23:43 -0300

    feat: alert when a invalid number is provided

--- screens/start-game/start-game.screen.tsx
+++ screens/start-game/start-game.screen.tsx
@@ -6,6 +6,7 @@ import {
 	Button,
 	TouchableWithoutFeedback,
 	Keyboard,
+	Alert,
 } from 'react-native';
 
 import Card from '../../components/card/card.component';
@@ -30,8 +31,20 @@ const StartGameScreen: React.FunctionComponent<StartGameScreenProps> = () => {
 
 	const confirmInputHandler = () => {
 		const chosenNumber = parseInt(enteredValue);
-		if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99)
+		if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+			Alert.alert(
+				'Invalid number!',
+				'Number has to be between 1 and 99.',
+				[
+					{
+						text: 'Okay',
+						style: 'destructive',
+						onPress: resetInputHandler,
+					},
+				]
+			);
 			return;
+		}
 		setConfirmed(true);
 		setSelectedNumber(chosenNumber);
 		setEnteredValue('');
